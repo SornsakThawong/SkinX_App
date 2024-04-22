bbf84cda1eaa0cb3e3f065a5ba96c2ee83256292
ศรศักดิ์ ทาวงษ์ <thawong2541@gmail.com>
2024-04-22 14:44:59 +0700

    clear id

--- lib/src/core/config/app_config.dart
+++ lib/src/core/config/app_config.dart
@@ -1,8 +1,8 @@
 class AppConfig {
   final String baseAccountUrl = "https://accounts.spotify.com/api/token";
   final String baseUrl = "https://api.spotify.com/v1";
-  final String clientId = "66449d997fee4660b29e0036f39c96ce";
-  final String clientSecret = "669a72cbc51c425d9f92ba83cf1a69d9";
+  final String clientId = "-";
+  final String clientSecret = "-";
 
   static final AppConfig instance = AppConfig._internal();
 
